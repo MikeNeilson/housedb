8a7c57c974e094afffdf7bda2764c0c88245a4d8
Mike Neilson <m.allan.neilson@gmail.com>
2021-03-23 21:17:48 -0700

    initial placeholder for list of locations.

--- database/src/main/java/net/hobbyscience/housedb/HouseDb.java
+++ database/src/main/java/net/hobbyscience/housedb/HouseDb.java
@@ -2,11 +2,24 @@ package net.hobbyscience.housedb;
 
 import java.sql.Connection;
 import javax.sql.DataSource;
+import java.util.logging.*;
+import java.util.List;
+//import housedb.tables.Records.*;
 
 public class HouseDb {
+    private static final Logger logger = Logger.getLogger(HouseDb.class.getName());
     private DataSource ds = null;
 
     public HouseDb(DataSource ds ){
         this.ds = ds;
+    }
+
+    public List<String> getAllLocations() throws Exception{
+        try( Connection conn = ds.getConnection(); ){
+            return null;
+        } catch(Exception ex){
+            logger.info(ex.getLocalizedMessage());
+            throw ex;
+        }        
     }
 }
