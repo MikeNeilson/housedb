8b822d881eab9437676606b477db0786d68f307a
Mike Neilson <gamingmike@gmail.com>
2021-03-28 10:13:07 -0700

    tried a different method.

--- .github/workflows/initial_db_setup.sql
+++ .github/workflows/initial_db_setup.sql
@@ -1,4 +1,3 @@
 CREATE ROLE housedb_owner WITH PASSWORD 'housepassword' LOGIN;
 CREATE ROLE housedb_user WITH PASSWORD 'testpassword' LOGIN;
 CREATE DATABASE housedb OWNER housedb_owner;
-ALTER DATABASE housedb set search_path = housedb, "$user", public;
--- database/src/main/resources/db/migration/R__security.sql
+++ database/src/main/resources/db/migration/R__security.sql
@@ -7,10 +7,10 @@ BEGIN
     select 
         up.regex into l_regex 
         from 
-            user_permissions up
-        join users u on u.id = up.user_id
-        join permissions p on p.id = up.permission_id
-        join data_tables dt on dt.id = up.data_table_id
+            housedb.user_permissions up
+        join housedb.users u on u.id = up.user_id
+        join housedb.permissions p on p.id = up.permission_id
+        join housedb.data_tables dt on dt.id = up.data_table_id
         where 
             u.username = p_username
             and
@@ -44,7 +44,7 @@ BEGIN
     elsif l_dt_id is null THEN
         raise exception 'Unrecognized data table %', p_data_table;
     end if;
-    insert into user_permissions(user_id,permission_id,data_table_id,regex) values (l_userid,l_perm_id,l_dt_id,p_regex);
+    insert into housedb,user_permissions(user_id,permission_id,data_table_id,regex) values (l_userid,l_perm_id,l_dt_id,p_regex);
     return true;
 END;
 $$ language plpgsql
