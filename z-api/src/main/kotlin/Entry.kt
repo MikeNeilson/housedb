96485db35226c111db945f6c67c137568b8386ad
Mike Neilson <gamingmike@gmail.com>
2021-03-29 20:43:09 -0700

    system can no set user from a JWT

--- z-api/src/main/kotlin/Entry.kt
+++ z-api/src/main/kotlin/Entry.kt
@@ -5,6 +5,13 @@ import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
 import controllers.*;
 import org.apache.tomcat.jdbc.pool.DataSource;
+import java.util.Base64;
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.treeToValue
+
 
 
 fun main(args: Array<String>) {
@@ -29,7 +36,22 @@ fun main(args: Array<String>) {
            
         }.attribute(javax.sql.DataSource::class.java,ds)
         .before { ctx -> 
-            ctx.attribute("username","guest")
+            val header = ctx.header("Authorization")
+            if( header != null ){
+                // verification will be handled at the gateway
+                //val jwt = Jwts.parserBuilder().build().parseClaimsJws(ctx.header("Authorization"))
+                val jwt = header.split(".")
+                val jwtClaims = Base64.getDecoder().decode(jwt[1])
+                val om = ObjectMapper()
+                val jsonClaims = om.readTree(jwtClaims)
+                //val user = jwt.subject()
+                println(jsonClaims)
+                val user = om.treeToValue<String>(jsonClaims.get("sub"))
+                println(user);                
+                ctx.attribute("username",user);
+            } else {
+                ctx.attribute("username","guest")
+            }
         }
         .start(7000)        
     app.routes {
