e484be682fcf5fe31904082e2c34d87cf3ad2acf
Mike Neilson <gamingmike@gmail.com>
2021-04-04 17:00:22 -0700

    initial android app

---
+++ gardendbapp/src/main/java/com/example/gardendbapp/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.example.gardendbapp.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.example.gardendbapp.data.LoginDataSource;
+import com.example.gardendbapp.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
