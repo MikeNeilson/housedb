e31bdbdabfb7c566c7487ea12f6376babacf427c
Mike Neilson <gamingmike@gmail.com>
2021-05-03 20:30:02 -0700

    [database:timeseries] moved create trigger on timeseries_values to new ts view migration file.

--- database/src/main/resources/db/migration/Views/R__timeseries.sql
+++ database/src/main/resources/db/migration/Views/R__timeseries.sql
@@ -11,3 +11,7 @@ create or replace view housedb.timeseries_values as
       join housedb.catalog tscat on itv.timeseries_id = tscat.id
       join housedb.parameters parms on tscat.parameter = parms.name
       order by timeseries_id,date_time;
+
+drop trigger if exists insert_tsv_trigger on housedb.timeseries_values;
+create trigger insert_tsv_trigger instead of insert or update or delete on housedb.timeseries_values 
+    for each row execute procedure housedb_timeseries.insert_tsv();      
