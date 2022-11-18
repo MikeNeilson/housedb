33712c785caaeacc6aad2b7e75014cfe0e8c8d6c
Mike Neilson <m.allan.neilson@gmail.com>
2022-11-18 12:40:58 -0800

    going in entirely different direction.

--- cppapi/src/auth/app.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <request_logger.h>
-#include "auth.h"
-#include "database.h"
-#include <crow/middlewares/cookie_parser.h>
-
-
-using ApiApp = crow::App<crow::CookieParser,Auth,DatabaseSession,RequestLogger>;
